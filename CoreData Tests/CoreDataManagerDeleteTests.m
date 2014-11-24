5b1bd3e3f56e44f80672c250e59bd6d68fa9924f
Carl Hill-Popper <chillpop@gmail.com>
2014-11-24 13:54:19 -0600

    updates per code review

--- CoreData Tests/CoreDataManagerDeleteTests.m
+++ CoreData Tests/CoreDataManagerDeleteTests.m
@@ -47,93 +47,115 @@
 
 - (void)testDeleteObject
 {
+    VOKCoreDataManager *manager = [VOKCoreDataManager sharedInstance];
+    
     VIThing *thing = [self addThingWithName:@"Johnny Test" numberOfHats:5000];
     
     XCTAssert([[VOKCoreDataManager sharedInstance] countForClass:[VIThing class]] == 1);
     
-    [[VOKCoreDataManager sharedInstance] deleteObject:thing];
+    [manager deleteObject:thing];
     
-    XCTAssert([[VOKCoreDataManager sharedInstance] countForClass:[VIThing class]] == 0);
+    XCTAssert([manager countForClass:[VIThing class]] == 0);
 }
 
 - (void)testDeleteAllObjectsOfClass
 {
+    VOKCoreDataManager *manager = [VOKCoreDataManager sharedInstance];
+    
     NSInteger numObjects = 10;
-    [self addSequentialThingsWithBaseName:@"Thing" baseNumberOfHats:1 count:numObjects];
+    [self addSequentialThingsWithBaseName:@"Thing"
+                         baseNumberOfHats:1
+                                    count:numObjects];
     
-    XCTAssert([[VOKCoreDataManager sharedInstance] countForClass:[VIThing class]] == numObjects);
+    XCTAssert([manager countForClass:[VIThing class]] == numObjects);
     
-    [[VOKCoreDataManager sharedInstance] deleteAllObjectsOfClass:[VIThing class] context:nil];
+    [manager deleteAllObjectsOfClass:[VIThing class] context:nil];
     
-    XCTAssert([[VOKCoreDataManager sharedInstance] countForClass:[VIThing class]] == 0);
+    XCTAssert([manager countForClass:[VIThing class]] == 0);
 }
 
 - (void)testDeleteObjectsWithPredicateThatShouldDeleteNone
 {
-    NSInteger numObjects = 10;
-    [self addSequentialThingsWithBaseName:@"Thing" baseNumberOfHats:1 count:numObjects];
+    VOKCoreDataManager *manager = [VOKCoreDataManager sharedInstance];
     
-    XCTAssert([[VOKCoreDataManager sharedInstance] countForClass:[VIThing class]] == numObjects);
+    NSInteger numObjects = 10;
+    [self addSequentialThingsWithBaseName:@"Thing"
+                         baseNumberOfHats:1
+                                    count:numObjects];
+    
+    XCTAssert([manager countForClass:[VIThing class]] == numObjects);
     
     //this predicate shouldn't match any existing objects
     NSPredicate *tooManyHats = [NSPredicate predicateWithFormat:@"%K > %@", VOK_CDSELECTOR(numberOfHats), @(100)];
     
-    [[VOKCoreDataManager sharedInstance] deleteAllObjectsOfClass:[VIThing class]
-                                               matchingPredicate:tooManyHats
-                                                         context:nil];
+    [manager deleteAllObjectsOfClass:[VIThing class]
+                   matchingPredicate:tooManyHats
+                             context:nil];
     
     //so nothing should be deleted
-    XCTAssert([[VOKCoreDataManager sharedInstance] countForClass:[VIThing class]] == numObjects);
+    XCTAssert([manager countForClass:[VIThing class]] == numObjects);
 }
 
 - (void)testDeleteObjectsWithPredicateThatShouldDeleteOne
 {
-    NSInteger numObjects = 10;
-    [self addSequentialThingsWithBaseName:@"Thing" baseNumberOfHats:1 count:numObjects];
+    VOKCoreDataManager *manager = [VOKCoreDataManager sharedInstance];
     
-    XCTAssert([[VOKCoreDataManager sharedInstance] countForClass:[VIThing class]] == numObjects);
-
+    NSInteger numObjects = 10;
+    [self addSequentialThingsWithBaseName:@"Thing"
+                         baseNumberOfHats:1
+                                    count:numObjects];
+    
+    XCTAssert([manager countForClass:[VIThing class]] == numObjects);
+    
     NSPredicate *hasFiveHats = [NSPredicate predicateWithFormat:@"%K == %@", VOK_CDSELECTOR(numberOfHats), @(5)];
-    [[VOKCoreDataManager sharedInstance] deleteAllObjectsOfClass:[VIThing class]
-                                               matchingPredicate:hasFiveHats
-                                                         context:nil];
+    [manager deleteAllObjectsOfClass:[VIThing class]
+                   matchingPredicate:hasFiveHats
+                             context:nil];
     
     //1 thing should be deleted
-    XCTAssert([[VOKCoreDataManager sharedInstance] countForClass:[VIThing class]] == numObjects - 1);
+    XCTAssert([manager countForClass:[VIThing class]] == numObjects - 1);
 }
 
 - (void)testDeleteObjectsWithPredicateThatShouldDeleteSome
 {
-    NSInteger numObjects = 10;
-    [self addSequentialThingsWithBaseName:@"Thing" baseNumberOfHats:1 count:numObjects];
+    VOKCoreDataManager *manager = [VOKCoreDataManager sharedInstance];
     
-    XCTAssert([[VOKCoreDataManager sharedInstance] countForClass:[VIThing class]] == numObjects);
+    NSInteger numObjects = 10;
+    [self addSequentialThingsWithBaseName:@"Thing"
+                         baseNumberOfHats:1
+                                    count:numObjects];
+    
+    XCTAssert([manager countForClass:[VIThing class]] == numObjects);
     
     NSPredicate *hasAtLeastFiveHats = [NSPredicate predicateWithFormat:@"%K >= %@",
                                        VOK_CDSELECTOR(numberOfHats), @(5)];
-    [[VOKCoreDataManager sharedInstance] deleteAllObjectsOfClass:[VIThing class]
-                                               matchingPredicate:hasAtLeastFiveHats
-                                                         context:nil];
+    [manager deleteAllObjectsOfClass:[VIThing class]
+                   matchingPredicate:hasAtLeastFiveHats
+                             context:nil];
     
     //6 things should be deleted: [1 - 10] deleting [5 - 10] leaves [1 - 4]
-    XCTAssert([[VOKCoreDataManager sharedInstance] countForClass:[VIThing class]] == numObjects - 6);
+    XCTAssert([manager countForClass:[VIThing class]] == numObjects - 6);
 }
 
 - (void)testDeleteObjectsWithPredicateThatShouldDeleteAll
 {
-    NSInteger numObjects = 10;
-    [self addSequentialThingsWithBaseName:@"Thing" baseNumberOfHats:1 count:numObjects];
+    VOKCoreDataManager *manager = [VOKCoreDataManager sharedInstance];
     
-    XCTAssert([[VOKCoreDataManager sharedInstance] countForClass:[VIThing class]] == numObjects);
+    NSInteger numObjects = 10;
+    [self addSequentialThingsWithBaseName:@"Thing"
+                         baseNumberOfHats:1
+                                    count:numObjects];
+    
+    XCTAssert([manager countForClass:[VIThing class]] == numObjects);
     
     NSPredicate *lessThanFiftyHats = [NSPredicate predicateWithFormat:@"%K < %@",
-                                       VOK_CDSELECTOR(numberOfHats), @(50)];
-    [[VOKCoreDataManager sharedInstance] deleteAllObjectsOfClass:[VIThing class]
-                                               matchingPredicate:lessThanFiftyHats
-                                                         context:nil];
+                                      VOK_CDSELECTOR(numberOfHats), @(50)];
+    [manager deleteAllObjectsOfClass:[VIThing class]
+                   matchingPredicate:lessThanFiftyHats
+                             context:nil];
     
     //should have nothing left
-    XCTAssert([[VOKCoreDataManager sharedInstance] countForClass:[VIThing class]] == 0);
+    XCTAssert([manager countForClass:[VIThing class]] == 0);
 }
 
 @end
