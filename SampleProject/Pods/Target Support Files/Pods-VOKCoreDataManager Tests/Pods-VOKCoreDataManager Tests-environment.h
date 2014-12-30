614c3d5dd54d7ba79a4a1e490400605b096fa594
Isaac Greenspan <isaac.greenspan@vokalinteractive.com>
2014-12-30 14:56:06 -0600

    Bumping the podspec version to 1.1.0; reorganizing the pod into sub specs to resolve #15; reorganizing the project to follow the example of the cocoapod lib create template; reworking the sample projects to use cocoapods.

---
+++ SampleProject/Pods/Target Support Files/Pods-VOKCoreDataManager Tests/Pods-VOKCoreDataManager Tests-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Vokoder
+#define COCOAPODS_POD_AVAILABLE_Vokoder
+#define COCOAPODS_VERSION_MAJOR_Vokoder 1
+#define COCOAPODS_VERSION_MINOR_Vokoder 1
+#define COCOAPODS_VERSION_PATCH_Vokoder 0
+
+// Vokoder/Core
+#define COCOAPODS_POD_AVAILABLE_Vokoder_Core
+#define COCOAPODS_VERSION_MAJOR_Vokoder_Core 1
+#define COCOAPODS_VERSION_MINOR_Vokoder_Core 1
+#define COCOAPODS_VERSION_PATCH_Vokoder_Core 0
+
+// Vokoder/DataSources
+#define COCOAPODS_POD_AVAILABLE_Vokoder_DataSources
+#define COCOAPODS_VERSION_MAJOR_Vokoder_DataSources 1
+#define COCOAPODS_VERSION_MINOR_Vokoder_DataSources 1
+#define COCOAPODS_VERSION_PATCH_Vokoder_DataSources 0
+
+// Vokoder/DataSources/FetchedResults
+#define COCOAPODS_POD_AVAILABLE_Vokoder_DataSources_FetchedResults
+#define COCOAPODS_VERSION_MAJOR_Vokoder_DataSources_FetchedResults 1
+#define COCOAPODS_VERSION_MINOR_Vokoder_DataSources_FetchedResults 1
+#define COCOAPODS_VERSION_PATCH_Vokoder_DataSources_FetchedResults 0
+
+// Vokoder/DataSources/PagingFetchedResults
+#define COCOAPODS_POD_AVAILABLE_Vokoder_DataSources_PagingFetchedResults
+#define COCOAPODS_VERSION_MAJOR_Vokoder_DataSources_PagingFetchedResults 1
+#define COCOAPODS_VERSION_MINOR_Vokoder_DataSources_PagingFetchedResults 1
+#define COCOAPODS_VERSION_PATCH_Vokoder_DataSources_PagingFetchedResults 0
+
