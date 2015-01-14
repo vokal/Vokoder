a7c6f791619c1d65140aeb31c76e52ac07033981
Ellen Shapiro <designatednerd@gmail.com>
2015-01-14 11:59:59 -0600

    One more copyright update.

--- SampleProject/VIAppDelegate.m
+++ SampleProject/VIAppDelegate.m
@@ -3,7 +3,7 @@
 //  CoreData
 //
 //  Created by Anthony Alesia on 7/26/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Vokal. All rights reserved.
 //
 
 #import "VIAppDelegate.h"
