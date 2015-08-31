7373b2f9a99c014ee8bd739d941d7443cb5e5532
Isaac Greenspan <isaac.greenspan@vokalinteractive.com>
2015-08-31 17:35:33 -0500

    Add mappable-model sub-entity of VIPerson.

---
+++ SampleProject/Models/VIMappablePerson.h
@@ -0,0 +1,13 @@
+//
+//  VIMappablePerson.h
+//  SampleProject
+//
+//  Created by Isaac Greenspan on 8/31/15.
+//
+//
+
+#import "VIPerson.h"
+
+@interface VIMappablePerson : VIPerson
+
+@end
---
+++ SampleProject/Models/VIMappablePerson.m
@@ -0,0 +1,42 @@
+//
+//  VIMappablePerson.m
+//  SampleProject
+//
+//  Created by Isaac Greenspan on 8/31/15.
+//
+//
+
+#import "VIMappablePerson.h"
+
+#import <VOKMappableModel.h>
+
+@interface VIMappablePerson () <VOKMappableModel>
+
+@end
+
+@implementation VIMappablePerson
+
+#pragma mark - VOKMappableModel
+
++ (NSArray *)coreDataMaps
+{
+    NSDateFormatter *df = [[NSDateFormatter alloc] init];
+    [df setDateFormat:@"dd' 'LLL' 'yy' 'HH:mm"];
+    [df setTimeZone:[NSTimeZone localTimeZone]];
+    return @[
+             VOK_MAP_FOREIGN_TO_LOCAL(@"first", firstName),
+             VOK_MAP_FOREIGN_TO_LOCAL(@"last", lastName),
+             [VOKManagedObjectMap mapWithForeignKeyPath:@"date_of_birth"
+                                            coreDataKey:VOK_CDSELECTOR(birthDay)
+                                          dateFormatter:df],
+             VOK_MAP_FOREIGN_TO_LOCAL(@"cat_num", numberOfCats),
+             VOK_MAP_FOREIGN_TO_LOCAL(@"CR_PREF", lovesCoolRanch),
+             ];
+}
+
++ (NSString *)uniqueKey
+{
+    return nil;
+}
+
+@end
