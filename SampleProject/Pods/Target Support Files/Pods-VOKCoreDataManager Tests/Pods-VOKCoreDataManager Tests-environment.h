fab8765666ec7b46a1d54568984f3bf34e5433a4
Isaac Greenspan <isaac.greenspan@vokalinteractive.com>
2015-09-16 11:17:49 -0500

    Run pod update Vokoder in both example projects.

--- SampleProject/Pods/Target Support Files/Pods-VOKCoreDataManager Tests/Pods-VOKCoreDataManager Tests-environment.h
+++ SampleProject/Pods/Target Support Files/Pods-VOKCoreDataManager Tests/Pods-VOKCoreDataManager Tests-environment.h
@@ -16,35 +16,35 @@
 #define COCOAPODS_POD_AVAILABLE_Vokoder
 #define COCOAPODS_VERSION_MAJOR_Vokoder 1
 #define COCOAPODS_VERSION_MINOR_Vokoder 3
-#define COCOAPODS_VERSION_PATCH_Vokoder 1
+#define COCOAPODS_VERSION_PATCH_Vokoder 2
 
 // Vokoder/Core
 #define COCOAPODS_POD_AVAILABLE_Vokoder_Core
 #define COCOAPODS_VERSION_MAJOR_Vokoder_Core 1
 #define COCOAPODS_VERSION_MINOR_Vokoder_Core 3
-#define COCOAPODS_VERSION_PATCH_Vokoder_Core 1
+#define COCOAPODS_VERSION_PATCH_Vokoder_Core 2
 
 // Vokoder/DataSources
 #define COCOAPODS_POD_AVAILABLE_Vokoder_DataSources
 #define COCOAPODS_VERSION_MAJOR_Vokoder_DataSources 1
 #define COCOAPODS_VERSION_MINOR_Vokoder_DataSources 3
-#define COCOAPODS_VERSION_PATCH_Vokoder_DataSources 1
+#define COCOAPODS_VERSION_PATCH_Vokoder_DataSources 2
 
 // Vokoder/DataSources/Collection
 #define COCOAPODS_POD_AVAILABLE_Vokoder_DataSources_Collection
 #define COCOAPODS_VERSION_MAJOR_Vokoder_DataSources_Collection 1
 #define COCOAPODS_VERSION_MINOR_Vokoder_DataSources_Collection 3
-#define COCOAPODS_VERSION_PATCH_Vokoder_DataSources_Collection 1
+#define COCOAPODS_VERSION_PATCH_Vokoder_DataSources_Collection 2
 
 // Vokoder/DataSources/FetchedResults
 #define COCOAPODS_POD_AVAILABLE_Vokoder_DataSources_FetchedResults
 #define COCOAPODS_VERSION_MAJOR_Vokoder_DataSources_FetchedResults 1
 #define COCOAPODS_VERSION_MINOR_Vokoder_DataSources_FetchedResults 3
-#define COCOAPODS_VERSION_PATCH_Vokoder_DataSources_FetchedResults 1
+#define COCOAPODS_VERSION_PATCH_Vokoder_DataSources_FetchedResults 2
 
 // Vokoder/DataSources/PagingFetchedResults
 #define COCOAPODS_POD_AVAILABLE_Vokoder_DataSources_PagingFetchedResults
 #define COCOAPODS_VERSION_MAJOR_Vokoder_DataSources_PagingFetchedResults 1
 #define COCOAPODS_VERSION_MINOR_Vokoder_DataSources_PagingFetchedResults 3
-#define COCOAPODS_VERSION_PATCH_Vokoder_DataSources_PagingFetchedResults 1
+#define COCOAPODS_VERSION_PATCH_Vokoder_DataSources_PagingFetchedResults 2
 
