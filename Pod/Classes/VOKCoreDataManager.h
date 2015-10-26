245f432b5542e58963ec5cc80b247227eb9b6457
Carl Hill-Popper <chillpop@gmail.com>
2015-10-26 16:19:18 -0500

    rearrange imports to avoid warning

--- Pod/Classes/VOKCoreDataManager.h
+++ Pod/Classes/VOKCoreDataManager.h
@@ -3,12 +3,12 @@
 //  VOKCoreData
 //
 
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
 #ifndef __IPHONE_7_0
 #warning "VOKCoreDataManager uses features only available in iOS SDK 7.0 and later."
 #endif
-
-#import <Foundation/Foundation.h>
-#import <CoreData/CoreData.h>
 
 #import "VOKCoreDataCollectionTypes.h"
 #import "VOKNullabilityFeatures.h"
