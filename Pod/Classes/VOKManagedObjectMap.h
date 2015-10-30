0fba4af271c10380c48fb551cbc2a81e7ccec211
Carl Hill-Popper <chillpop@gmail.com>
2015-10-30 13:20:05 -0500

    remove number formatter macro
    it will be implemented in the automatic mapping in a future commit.

--- Pod/Classes/VOKManagedObjectMap.h
+++ Pod/Classes/VOKManagedObjectMap.h
@@ -37,16 +37,6 @@
 #   define VOK_MAP_FOREIGN_TO_LOCAL(inputKeyPath, coreDataSelectorSymbol) [VOKManagedObjectMap mapWithForeignKeyPath:inputKeyPath coreDataKey:VOK_CDSELECTOR(coreDataSelectorSymbol)]
 #endif
 
-/**
- *  Creates a map for a string to a number using a default NSNumberFormatter.
- *
- *  @param inputKeyPath             The foreign key to match with the local key.
- *  @param coreDataSelectorSymbol   The local selector symbol
- */
-#ifndef VOK_MAP_FOREIGN_STRING_TO_LOCAL_NUMBER
-#   define VOK_MAP_FOREIGN_STRING_TO_LOCAL_NUMBER(inputKeyPath, coreDataSelectorSymbol) [VOKManagedObjectMap mapWithForeignKeyPath:inputKeyPath coreDataKey:VOK_CDSELECTOR(coreDataSelectorSymbol) numberFormatter:[VOKManagedObjectMap vok_defaultNumberFormatter]]
-#endif
-
 NS_ASSUME_NONNULL_BEGIN
 
 @interface VOKManagedObjectMap : NSObject
