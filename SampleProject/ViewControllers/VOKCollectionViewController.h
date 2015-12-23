6bbb3a705b743838519d648709263e552bd3c300
Sean Wolter <sean.wolter@vokalinteractive.com>
2015-12-23 07:39:23 -0600

    moved view controllers into a folder

---
+++ SampleProject/ViewControllers/VOKCollectionViewController.h
@@ -0,0 +1,12 @@
+//
+//  VOKCollectionViewController.h
+//  Vokoder Sample Project
+//
+//  Copyright © 2015 Vokal.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VOKCollectionViewController : UICollectionViewController
+
+@end
---
+++ SampleProject/ViewControllers/VOKTableViewController.h
@@ -0,0 +1,12 @@
+//
+//  VOKTableViewController.h
+//  Vokoder Sample Project
+//
+//  Copyright © 2015 Vokal.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VOKTableViewController : UITableViewController
+
+@end
