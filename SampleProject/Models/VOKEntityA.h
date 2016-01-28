3048021dd7ff3cbfb74b27cd50f9ab469f549ccd
Isaac Greenspan <isaac.greenspan@vokalinteractive.com>
2016-01-28 12:28:37 -0600

    Add test of vok_entityName.

---
+++ SampleProject/Models/VOKEntityA.h
@@ -0,0 +1,18 @@
+//
+//  VOKEntityA.h
+//  
+//
+//  Created by Isaac Greenspan on 1/28/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VOKEntityA : NSManagedObject
+
+@end
+
+NS_ASSUME_NONNULL_END
