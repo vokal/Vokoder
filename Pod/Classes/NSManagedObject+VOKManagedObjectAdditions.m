vokal/Vokoder

commit da3bf047da6b180fa265232c9bceb1c2248b4db4

modernize syntax: use dot notation

Pod/Classes/NSManagedObject+VOKManagedObjectAdditions.m
@@ -91,12 +91,8 @@
 
     NSArray *array = [[VOKCoreDataManager sharedInstance] importArray:@[inputDict]
                                                              forClass:[self class]
-                                                          withContext:contextOrNil];    
-    if ([array count]) {
-        return [array firstObject];
-    } else {
-        return nil;
-    }
+                                                          withContext:contextOrNil];
+    return array.firstObject;
 }
 
 + (void)vok_addWithArrayInBackground:(NSArray *)inputArray completion:(VOKManagedObjectsReturnBlock)completion
@@ -126,7 +122,7 @@
 + (NSFetchRequest *)vok_fetchRequestWithPredicate:(NSPredicate *)predicate
 {
     NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:[self vok_entityName]];
-    [fetchRequest setPredicate:predicate];
+    fetchRequest.predicate = predicate;
     return fetchRequest;
 }
 
@@ -172,13 +168,8 @@
 {
     NSArray *results = [self vok_fetchAllForPredicate:predicate forManagedObjectContext:contextOrNil];
 
-    NSUInteger count = [results count];
-    if (count) {
-        NSAssert(count == 1, @"Your predicate is returning more than 1 object, but the coredatamanager returns only one.");
-        return [results lastObject];
-    }
-
-    return nil;
+    NSAssert(results.count == 1, @"Your predicate is returning more than 1 object, but the coredatamanager returns only one.");
+    return results.lastObject;
 }
 
 @end

Pod/Classes/Optional Data Sources/VOKCollectionDataSource.m
@@ -10,10 +10,10 @@
 #import "VOKCoreDataManager.h"
 #import "VOKCoreDataManagerInternalMacros.h"
 
-@interface VOKCollectionDataSource () {
-    NSMutableArray *_objectChanges;
-    NSMutableArray *_sectionChanges;
-}
+@interface VOKCollectionDataSource ()
+
+@property (nonatomic, strong) NSMutableArray *objectChanges;
+@property (nonatomic, strong) NSMutableArray *sectionChanges;
 
 @end
 
@@ -29,11 +29,11 @@
              fetchLimit:(NSInteger)fetchLimit
                delegate:(id <VOKFetchedResultsDataSourceDelegate>)delegate
 {
-    self.collectionView = collectionView;
-    self.collectionView.delegate = self;
-    self.collectionView.dataSource = self;
-    _objectChanges = [@[] mutableCopy];
-    _sectionChanges = [@[] mutableCopy];
+    _collectionView = collectionView;
+    _collectionView.delegate = self;
+    _collectionView.dataSource = self;
+    _objectChanges = [NSMutableArray array];
+    _sectionChanges = [NSMutableArray array];
 
     return [self initWithPredicate:predicate
                          cacheName:cacheName
@@ -127,17 +127,17 @@
 - (void)reloadData
 {
     NSError *error = nil;
-    if (![_fetchedResultsController performFetch:&error]) {
+    if (![self.fetchedResultsController performFetch:&error]) {
         VOK_CDLog(@"Unresolved error %@, %@", error, [error userInfo]);
         abort();
     }
     //FOR REVIEW controllerWillChangeContent is not being called in tests - this updates the table explicitly
-    [_collectionView reloadData];
+    [self.collectionView reloadData];
 }
 
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
 {
-    NSInteger sectionCount = [[self.fetchedResultsController sections] count];
+    NSInteger sectionCount = self.fetchedResultsController.sections.count;
 
     // If there are no sections, the numberOfItemsInSection: method is never called,
     // so the delegeate fetchResultsDataSourceHasResults: method isn't called
@@ -150,11 +150,11 @@
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    id<NSFetchedResultsSectionInfo> sectionInfo = [self.fetchedResultsController sections][section];
+    id<NSFetchedResultsSectionInfo> sectionInfo = self.fetchedResultsController.sections[section];
 
     // NSFetchedResultsController doesn't really respect fetchLimit, so we have
     // to work around it: don't allow more items than the limit.
-    NSInteger resultCount = [sectionInfo numberOfObjects];
+    NSInteger resultCount = sectionInfo.numberOfObjects;
     if (self.fetchedResultsController.fetchRequest.fetchLimit > 0
         && resultCount > self.fetchedResultsController.fetchRequest.fetchLimit) {
         resultCount = self.fetchedResultsController.fetchRequest.fetchLimit;
@@ -196,8 +196,10 @@
 
 #pragma mark - Fetched results controller
 
-- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
-           atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type
+- (void)controller:(NSFetchedResultsController *)controller
+  didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
+           atIndex:(NSUInteger)sectionIndex
+     forChangeType:(NSFetchedResultsChangeType)type
 {
     
     NSMutableDictionary *change = [NSMutableDictionary new];
@@ -214,11 +216,13 @@
             break;
     }
     
-    [_sectionChanges addObject:change];
+    [self.sectionChanges addObject:change];
 }
 
-- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject
-       atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type
+- (void)controller:(NSFetchedResultsController *)controller
+   didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)indexPath
+     forChangeType:(NSFetchedResultsChangeType)type
       newIndexPath:(NSIndexPath *)newIndexPath
 {
     
@@ -237,15 +241,15 @@
             change[@(type)] = @[indexPath, newIndexPath];
             break;
     }
-    [_objectChanges addObject:change];
+    [self.objectChanges addObject:change];
 }
 
 - (void)controllerDidChangeContent:(NSFetchedResultsController *)controller
 {
-    if ([_sectionChanges count] > 0) {
+    if (self.sectionChanges.count > 0) {
         [self.collectionView performBatchUpdates:^{
             
-            for (NSDictionary *change in _sectionChanges) {
+            for (NSDictionary *change in self.sectionChanges) {
                 [change enumerateKeysAndObjectsUsingBlock:^(NSNumber *key, id obj, BOOL *stop) {
                     NSIndexPath *indexPath, *newIndexPath;
                     if ([obj isKindOfClass:[NSIndexPath class]]) {
@@ -260,7 +264,7 @@
                     // limit before acting on the change.
                     NSUInteger fetchLimit = controller.fetchRequest.fetchLimit;
 
-                    NSFetchedResultsChangeType type = [key unsignedIntegerValue];
+                    NSFetchedResultsChangeType type = key.unsignedIntegerValue;
                     switch (type) {
                         case NSFetchedResultsChangeInsert:
                             if (fetchLimit == 0 || indexPath.row < fetchLimit) {
@@ -302,7 +306,7 @@
         } completion:nil];
     }
 
-    if ([_objectChanges count] > 0 && [_sectionChanges count] == 0) {
+    if (self.objectChanges.count > 0 && self.sectionChanges.count == 0) {
         
         if ([self shouldReloadCollectionViewToPreventKnownIssue]) {
             // This is to prevent a bug in UICollectionView from occurring.
@@ -316,7 +320,7 @@
 
             [self.collectionView performBatchUpdates:^{
 
-                for (NSDictionary *change in _objectChanges) {
+                for (NSDictionary *change in self.objectChanges) {
                     [change enumerateKeysAndObjectsUsingBlock:^(NSNumber *key, id obj, BOOL *stop) {
                         NSIndexPath *indexPath, *newIndexPath;
                         if ([obj isKindOfClass:[NSIndexPath class]]) {
@@ -331,7 +335,7 @@
                         // limit before acting on the change.
                         NSUInteger fetchLimit = controller.fetchRequest.fetchLimit;
 
-                        NSFetchedResultsChangeType type = [key unsignedIntegerValue];
+                        NSFetchedResultsChangeType type = key.unsignedIntegerValue;
                         switch (type) {
                             case NSFetchedResultsChangeInsert:
                                 if (fetchLimit == 0 || indexPath.row < fetchLimit) {
@@ -374,16 +378,16 @@
         }
     }
     
-    [_sectionChanges removeAllObjects];
-    [_objectChanges removeAllObjects];
+    [self.sectionChanges removeAllObjects];
+    [self.objectChanges removeAllObjects];
 }
 
 - (BOOL)shouldReloadCollectionViewToPreventKnownIssue
 {
     __block BOOL shouldReload = NO;
-    for (NSDictionary *change in _objectChanges) {
-        [change enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
-            NSFetchedResultsChangeType type = [key unsignedIntegerValue];
+    for (NSDictionary *change in self.objectChanges) {
+        [change enumerateKeysAndObjectsUsingBlock:^(NSNumber *key, id obj, BOOL *stop) {
+            NSFetchedResultsChangeType type = key.unsignedIntegerValue;
             NSIndexPath *indexPath = obj;
             switch (type) {
                 case NSFetchedResultsChangeInsert:

Pod/Classes/Optional Data Sources/VOKFetchedResultsDataSource.m
@@ -184,7 +184,7 @@
         abort();
     }
     //FOR TESTING ONLY, NOT NECESSARY
-    [_tableView reloadData];
+    [self.tableView reloadData];
 }
 
 - (NSArray *)fetchedObjects
@@ -201,8 +201,8 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    if ([_delegate respondsToSelector:@selector(fetchResultsDataSourceSelectedObject:)]) {
-        [_delegate fetchResultsDataSourceSelectedObject:[_fetchedResultsController objectAtIndexPath:indexPath]];
+    if ([self.delegate respondsToSelector:@selector(fetchResultsDataSourceSelectedObject:)]) {
+        [self.delegate fetchResultsDataSourceSelectedObject:[_fetchedResultsController objectAtIndexPath:indexPath]];
     }
 
     if (self.clearsTableViewCellSelection) {
@@ -212,8 +212,8 @@
 
 - (void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    if ([_delegate respondsToSelector:@selector(fetchResultsDataSourceDeselectedObject:)]) {
-        [_delegate fetchResultsDataSourceDeselectedObject:[_fetchedResultsController objectAtIndexPath:indexPath]];
+    if ([self.delegate respondsToSelector:@selector(fetchResultsDataSourceDeselectedObject:)]) {
+        [self.delegate fetchResultsDataSourceDeselectedObject:[_fetchedResultsController objectAtIndexPath:indexPath]];
     }
 }
 
@@ -221,24 +221,24 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    NSInteger sectionCount = [[_fetchedResultsController sections] count];
+    NSInteger sectionCount = _fetchedResultsController.sections.count;
 
     // If there are no sections, the numberOfRowsInSection: method is never called,
     // so the delegeate fetchResultsDataSourceHasResults: method isn't called
     // with NO. Do so here, if necessary.
-    if (sectionCount == 0 && [_delegate respondsToSelector:@selector(fetchResultsDataSourceHasResults:)]) {
-        [_delegate fetchResultsDataSourceHasResults:NO];
+    if (sectionCount == 0 && [self.delegate respondsToSelector:@selector(fetchResultsDataSourceHasResults:)]) {
+        [self.delegate fetchResultsDataSourceHasResults:NO];
     }
     return sectionCount;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    id <NSFetchedResultsSectionInfo> sectionInfo = [_fetchedResultsController sections][section];
+    id <NSFetchedResultsSectionInfo> sectionInfo = _fetchedResultsController.sections[section];
 
     // NSFetchedResultsController doesn't really respect fetchLimit, so we have
     // to work around it: don't allow more items than the limit.
-    NSInteger resultCount = [sectionInfo numberOfObjects];
+    NSInteger resultCount = sectionInfo.numberOfObjects;
     if (self.fetchedResultsController.fetchRequest.fetchLimit > 0
         && resultCount > self.fetchedResultsController.fetchRequest.fetchLimit) {
         resultCount = self.fetchedResultsController.fetchRequest.fetchLimit;
@@ -276,17 +276,17 @@
     NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
     NSEntityDescription *entity = [NSEntityDescription entityForName:[_managedObjectClass vok_entityName]
                                               inManagedObjectContext:_managedObjectContext];
-    [fetchRequest setEntity:entity];
+    fetchRequest.entity = entity;
     
-    [fetchRequest setFetchBatchSize:_batchSize];
+    fetchRequest.fetchBatchSize = _batchSize;
     
-    [fetchRequest setFetchLimit:_fetchLimit];
+    fetchRequest.fetchLimit = _fetchLimit;
 
-    [fetchRequest setSortDescriptors:_sortDescriptors];
+    fetchRequest.sortDescriptors = _sortDescriptors;
 
-    [fetchRequest setPredicate:_predicate];
+    fetchRequest.predicate = _predicate;
     
-    [fetchRequest setIncludesSubentities:_includesSubentities];
+    fetchRequest.includesSubentities = _includesSubentities;
 
     NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
                                                                                                 managedObjectContext:_managedObjectContext
@@ -302,7 +302,7 @@
 
 - (void)controllerWillChangeContent:(NSFetchedResultsController *)controller
 {
-    [_tableView beginUpdates];
+    [self.tableView beginUpdates];
 }
 
 - (void)controller:(NSFetchedResultsController *)controller
@@ -312,11 +312,11 @@
 {
     switch (type) {
         case NSFetchedResultsChangeInsert:
-            [_tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
             break;
 
         case NSFetchedResultsChangeDelete:
-            [_tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
             break;
         default:
             //Do nothing, shut up the compiler.
@@ -338,19 +338,19 @@
     switch (type) {
         case NSFetchedResultsChangeInsert:
             if (fetchLimit == 0 || newIndexPath.row < fetchLimit) {
-                [_tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+                [self.tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
             }
             break;
 
         case NSFetchedResultsChangeDelete:
             if (fetchLimit == 0 || indexPath.row < fetchLimit) {
-                [_tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+                [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
             }
             break;
 
         case NSFetchedResultsChangeUpdate:
             if (fetchLimit == 0 || indexPath.row < fetchLimit) {
-                [_tableView reloadRowsAtIndexPaths:@[indexPath]
+                [self.tableView reloadRowsAtIndexPaths:@[indexPath]
                                   withRowAnimation:UITableViewRowAnimationNone];
             }
             break;
@@ -359,21 +359,21 @@
             if (fetchLimit > 0) {
                 if (indexPath.row < fetchLimit && newIndexPath.row < fetchLimit) {
                     // Before and after are both in range
-                    [_tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-                    [_tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+                    [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+                    [self.tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
                 } else if (indexPath.row >= fetchLimit && newIndexPath.row >= fetchLimit) {
                     // Both out of range: do nothing
                 } else if (indexPath.row < fetchLimit && newIndexPath.row >= fetchLimit) {
                     // Destination is out of range: remove the original row
-                    [_tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+                    [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
                 } else if (indexPath.row >= fetchLimit && newIndexPath.row < fetchLimit) {
                     // Origin is out of range: add the new row
-                    [_tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+                    [self.tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
                 }
             } else {
                 // No fetch limit: behave normally
-                [_tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-                [_tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+                [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+                [self.tableView insertRowsAtIndexPaths:@[newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
             }
             break;
     }
@@ -381,14 +381,14 @@
 
 - (void)controllerDidChangeContent:(NSFetchedResultsController *)controller
 {
-    [_tableView endUpdates];
+    [self.tableView endUpdates];
 }
 
 - (UITableViewCell *)cellAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"CellIdentifier";
 
-    UITableViewCell *cell = [_tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:CellIdentifier];
 
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];

Pod/Classes/VOKCoreDataManager.m
@@ -278,7 +278,7 @@ static VOKCoreDataManager *VOK_SharedObject;
         
         NSPredicate *predicate = [NSPredicate predicateWithFormat:@"%K == %@", mapper.uniqueComparisonKey, [inputDict valueForKeyPath:mapper.foreignUniqueComparisonKey]];
         NSArray *matchingObjects = [existingObjectArray filteredArrayUsingPredicate:predicate];
-        NSUInteger matchingObjectsCount = [matchingObjects count];
+        NSUInteger matchingObjectsCount = matchingObjects.count;
         
         if (matchingObjectsCount) {
             NSAssert(matchingObjectsCount < 2, @"UNIQUE IDENTIFIER IS NOT UNIQUE. MORE THAN ONE MATCHING OBJECT FOUND");

Pod/Classes/VOKManagedObjectMap.m
@@ -53,8 +53,8 @@
     static NSDateFormatter *DefaultDateFormatter;
     dispatch_once(&pred, ^{
         DefaultDateFormatter = [NSDateFormatter new];
-        [DefaultDateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'"];
-        [DefaultDateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+        DefaultDateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'";
+        DefaultDateFormatter.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
     });
 
     return DefaultDateFormatter;
@@ -66,9 +66,9 @@
     static NSDateFormatter *DateFormatterWithoutMicroseconds;
     dispatch_once(&formatter_dispatch_token, ^{
         DateFormatterWithoutMicroseconds = [NSDateFormatter new];
-        [DateFormatterWithoutMicroseconds setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
-        [DateFormatterWithoutMicroseconds setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
-    });
+        DateFormatterWithoutMicroseconds.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+        DateFormatterWithoutMicroseconds.timeZone = [NSTimeZone timeZoneForSecondsFromGMT:0];
+         });
 
     return DateFormatterWithoutMicroseconds;
 }

Pod/Classes/VOKManagedObjectMapper.m
@@ -30,8 +30,8 @@
 + (instancetype)mapperWithUniqueKey:(NSString *)comparisonKey andMaps:(NSArray *)mapsArray;
 {
     VOKManagedObjectMapper *mapper = [[self alloc] init];
-    [mapper setMapsArray:mapsArray];
-    [mapper setUniqueComparisonKey:comparisonKey];
+    mapper.mapsArray = mapsArray;
+    mapper.uniqueComparisonKey = comparisonKey;
     return mapper;
 }
 
@@ -185,11 +185,11 @@
 
 - (Class)expectedClassForObject:(NSManagedObject *)object andKey:(id)key
 {
-    NSDictionary *attributes = [[object entity] attributesByName];
+    NSDictionary *attributes = object.entity.attributesByName;
     NSAttributeDescription *attributeDescription = [attributes valueForKey:key];
-    NSString *className = [attributeDescription attributeValueClassName];
+    NSString *className = attributeDescription.attributeValueClassName;
     if (!className) {
-        const char *className = [[object.entity managedObjectClassName] cStringUsingEncoding:NSUTF8StringEncoding];
+        const char *className = [object.entity.managedObjectClassName cStringUsingEncoding:NSUTF8StringEncoding];
         const char *propertyName = [key cStringUsingEncoding:NSUTF8StringEncoding];
 
         Class managedObjectClass = objc_getClass(className);
@@ -293,7 +293,7 @@ static NSString *const period = @".";
 - (void)createNestedDictionary:(NSMutableDictionary *)outputDict fromKeyPathComponents:(NSArray *)components
 {
     __block NSMutableDictionary *nestedDict = outputDict;
-    NSUInteger lastObjectIndex = [components count] - 1;
+    NSUInteger lastObjectIndex = components.count - 1;
     [components enumerateObjectsUsingBlock:^(NSString *keyPathComponent, NSUInteger idx, BOOL *stop) {
         if (![nestedDict valueForKey:keyPathComponent] && idx < lastObjectIndex) {
             nestedDict[keyPathComponent] = [NSMutableDictionary dictionary];
@@ -330,7 +330,7 @@ static NSString *const period = @".";
 
 - (NSDictionary *)dictionaryRepresentationOfManagedObject:(NSManagedObject *)object
 {
-    NSDictionary *attributes = [[object entity] attributesByName];
+    NSDictionary *attributes = object.entity.attributesByName;
     NSMutableDictionary *outputDict = [NSMutableDictionary new];
     [attributes enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
         id outputObject = [object valueForKey:key];

commit 7ed4918e1c32875da11f6aef0ef0c57da1b0eef3

Fetch can return no objects (broke in da3bf047da6b180fa265232c9bceb1c2248b4db4)

Pod/Classes/NSManagedObject+VOKManagedObjectAdditions.m
@@ -184,7 +184,7 @@
 {
     NSArray *results = [self vok_fetchAllForPredicate:predicate forManagedObjectContext:contextOrNil];
 
-    NSAssert(results.count == 1, @"Your predicate is returning more than 1 object, but the coredatamanager returns only one.");
+    NSAssert(results.count <= 1, @"Your predicate is returning more than 1 object, but the coredatamanager returns only one.");
     return results.lastObject;
 }
 