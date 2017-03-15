083232796ab86be10218b1bffead843d1b037cdc
Ellen Shapiro <designatednerd@gmail.com>
2017-03-15 12:20:36 -0500

    Add watch app/extension

---
+++ SampleProject/WatchApp Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  WatchApp Extension
+//
+//  Created by Ellen Shapiro (Work) on 3/15/17.
+//  Copyright © 2017 Vokal. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ SampleProject/WatchApp Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  WatchApp Extension
+//
+//  Created by Ellen Shapiro (Work) on 3/15/17.
+//  Copyright © 2017 Vokal. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
