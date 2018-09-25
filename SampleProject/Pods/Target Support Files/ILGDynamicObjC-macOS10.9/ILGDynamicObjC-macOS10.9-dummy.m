a766eca6f18f6db310861bd0f0d4cf231cae9f47
Kyle Haptonstall <khaptonstall93@gmail.com>
2018-09-25 17:48:28 -0500

    ran pod install

---
+++ SampleProject/Pods/Target Support Files/ILGDynamicObjC-macOS10.9/ILGDynamicObjC-macOS10.9-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ILGDynamicObjC_macOS10_9 : NSObject
+@end
+@implementation PodsDummy_ILGDynamicObjC_macOS10_9
+@end
--- SampleProject/Pods/Target Support Files/Vokoder.root-Core-MapperMacros-OSX/Vokoder.root-Core-MapperMacros-OSX-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Vokoder_root_Core_MapperMacros_OSX : NSObject
-@end
-@implementation PodsDummy_Vokoder_root_Core_MapperMacros_OSX
-@end
